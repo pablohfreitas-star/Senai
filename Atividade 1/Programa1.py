9adebf0f805b10455df6017a713c1d17727d7494
pablohfreitas-star <pablo_h_freitas@estudante.sesisenai.org.br>
2026-02-27 19:43:59 +0000

    update

---
+++ Atividade 1/Programa1.py
@@ -0,0 +1,4 @@
+a = 10 #Inserir o valor 10 na variável
+b = 20
+c = a + b
+print("a soma dos dois valores",c)
---
+++ Atividade 1/Progrma4.py
@@ -0,0 +1,12 @@
+nome = input("insira o seu nome")
+nota1 = float(input("insira a primeira nota"))
+nota2 = float(input("insira a segunda nota"))
+media = (nota1 + nota2) / 2
+print(" A média das notas é:", media)
+if media >= 7:
+    print("O aluno esta aprovado")
+else:
+     print("O aluno esta reprovado")
+     
+
+    

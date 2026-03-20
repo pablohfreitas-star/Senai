a4e0782b29dfcff8a7ba3b95af913aa622684597
pablohfreitas-star <pablo_h_freitas@estudante.sesisenai.org.br>
2026-03-20 19:09:35 +0000

    update

---
+++ Atividade 1/Progrma5.py
@@ -0,0 +1,10 @@
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+
+if idade > 0
+    print("idade inválida! tente novamente com um valor igual ou maior que 0")
+if idade < 120
+    print("idade invalida! tente novamente com um valor igual ou menor que 120")
+
+dias_de_vida = idade * 365
+print("voce ja viveu: ", dias_de_vida)
